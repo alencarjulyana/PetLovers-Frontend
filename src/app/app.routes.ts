9c6c18e464de74c934f55b2911006d86a042d091
alencarjulyana <julyanasalencar@gmail.com>
2025-01-29 07:24:28 -0300

    Cadastro Pets nao finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { CadastroPetsComponent } from './pages/cadastropets/cadastropets.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'cadastro', pathMatch: 'full' }, 
   { path: 'cadastro', component: CadastroComponent },
+  { path: 'perfil', component: CadastroPetsComponent },
   { path: '**', redirectTo: 'cadastro' }
 ];
+
--- src/app/pages/cadastropets/cadastropets.component.ts
+++ src/app/pages/cadastropets/cadastropets.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastropets',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastropets.component.html',
-  styleUrl: './cadastropets.component.css'
+  styleUrls: ['./cadastropets.component.css']
 })
-export class CadastropetsComponent {
+export class CadastroPetsComponent {
+  pet = {
+    tipo: '',
+    nome: '',
+    idade: '',
+    raca: '',
+    porte: '',
+    castrado: '',
+    sexo: ''
+  };
+
+  imagemPreview: string | ArrayBuffer | null = null;
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imagemPreview = e.target?.result ?? null;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  cadastrarPet() {
+    console.log('Pet cadastrado:', this.pet);
+    alert('Pet cadastrado com sucesso!');
+  }
 
 }
