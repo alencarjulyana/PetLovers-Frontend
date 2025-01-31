61ed1dc3da6b19f05f08e68a5f3407b6d3aac27f
alencarjulyana <julyanasalencar@gmail.com>
2025-01-30 22:52:14 -0300

    Tela de Listagem de Pets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { CadastroPetsComponent } from './pages/cadastropets/cadastropets.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'cadastro', pathMatch: 'full' }, 
+  { path: '', redirectTo: 'cadastro', pathMatch: 'full' },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'perfil', component: CadastroPetsComponent },
-  { path: '**', redirectTo: 'cadastro' }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', redirectTo: '/dashboard' }
+
 ];
 
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
+  standalone: true, // Mark as standalone
+  imports: [CommonModule, RouterModule], // Import necessary modules
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  pets: any[] = []; // Array to store all pets
+  filteredPets: any[] = []; // Array to store filtered pets
 
+  ngOnInit() {
+    this.loadPets(); // Load pets when the component initializes
+  }
+
+  
+  loadPets() {
+    const pets = localStorage.getItem('pets');
+    if (pets) {
+      this.pets = JSON.parse(pets);
+      this.filteredPets = this.pets;
+    }
+  }
+
+  filterPets(filter: string) {
+    this.filteredPets = this.pets.filter(pet =>
+      pet.tipo === filter || pet.sexo === filter || pet.porte === filter
+    );
+  }
+
+  // Reset filters and show all pets
+  resetFilters() {
+    this.filteredPets = this.pets;
+  }
 }
